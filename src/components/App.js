39fbb1819271e049af66716cb2cc391211d5be5d
pixelove <pixeloved@gmail.com>
2018-02-19 23:32:05 +0100

    just create-react-app

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Books I want to read (someday)</h1>
+        </header>
+        <p className="App-intro">
+          <strong>[placeholder for content]</strong>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
